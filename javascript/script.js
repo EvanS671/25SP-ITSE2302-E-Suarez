8555ac2be4673775df339f7b30e7e0eabd02af7c
EvanS671 <emsuarez@mymail.tstc.edu>
2025-09-12 13:45:21 -0500

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,20 @@
+var First = "Evan";
+var Last = "Suarez";
+var sentence =
+  "This is about my cat that I love very much named his name is Apollo and is around 2 years old.";
+var theTruth = true;
+var num1 = "10";
+var num2 = "15";
+var num3 = "25";
+var length = sentence.length;
+
+console.log(First + Last);
+console.log(sentence);
+console.log("The length of my sentence is: " + length + " characters long");
+console.log("The sum of " + num1 + " and " + num2 + " is equal to " + num3);
+console.log(
+  "The variable theTruth is storing the value " +
+    theTruth +
+    " The data type of theTruth is " +
+    typeof theTruth
+);
